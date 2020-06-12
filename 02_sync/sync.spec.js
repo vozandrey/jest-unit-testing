820a7c537ee5a314e62a66668bb4c471a9f1bd5a
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-13 00:06:02 +0300

    write function groupBy tests

--- 02_sync/sync.spec.js
+++ 02_sync/sync.spec.js
@@ -1,7 +1,17 @@
 const Lodash = require('./sync');
 
+let _ = new Lodash();
+
 describe('Lodash: compact', () => {
-    const _ = new Lodash();
+    let array;
+
+    beforeEach(() => {
+        array = [false, 42, 0, '', true, null, 'hello'];
+    });
+
+    afterAll(() => {
+        _ = new Lodash();
+    });
 
     test('should be defined', () => {
         expect(_.compact).toBeDefined();
@@ -9,16 +19,46 @@ describe('Lodash: compact', () => {
     });
 
     test('should remove falsy values from array', () => {
-        const array = [false, 42, 0, '', true, null, 'hello'];
         const result = [42, true, 'hello'];
         expect(_.compact(array)).toEqual(result);
     });
 
     test('should NOT contain falsy values', () => {
-        const array = [false, 42, 0, '', true, null, 'hello'];
         expect(_.compact(array)).not.toContain(false);
         expect(_.compact(array)).not.toContain(0);
         expect(_.compact(array)).not.toContain('');
         expect(_.compact(array)).not.toContain(null);
+    });
+});
+
+describe('Lodash: groupBy', () => {
+
+    test('should be defined', () => {
+        expect(_.groupBy).toBeDefined();
+        expect(_.groupBy).not.toBeUndefined();
+    });
+
+    test('should group array items by Math.floor', () => {
+        const array = [2.2, 2.4, 4.2, 3.1];
+        const result = {
+            2: [2.2, 2.4],
+            4: [4.2],
+            3: [3.1]
+        }
+
+        expect(_.groupBy(array, Math.floor)).toEqual(result);
+    });
+
+    test('should group array items by length', () => {
+        const array = ['one', 'two', 'three'];
+        const result = {
+            5: ['three'],
+            3: ['one', 'two']
+        }
+        expect(_.groupBy(array, 'length')).toEqual(result);
+    });
+
+    test('should NOT return array', () => {
+        expect(_.groupBy([], Math.trunc)).not.toBeInstanceOf(Array);
     });
 });

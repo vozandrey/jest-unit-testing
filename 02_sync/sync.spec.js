3f73f842103cf0675b0d1390b3f8fddc9b8b45d2
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-12 23:25:42 +0300

    test function compact

---
+++ 02_sync/sync.spec.js
@@ -0,0 +1,24 @@
+const Lodash = require('./sync');
+
+describe('Lodash: compact', () => {
+    const _ = new Lodash();
+
+    test('should be defined', () => {
+        expect(_.compact).toBeDefined();
+        expect(_.compact).not.toBeUndefined();
+    });
+
+    test('should remove falsy values from array', () => {
+        const array = [false, 42, 0, '', true, null, 'hello'];
+        const result = [42, true, 'hello'];
+        expect(_.compact(array)).toEqual(result);
+    });
+
+    test('should NOT contain falsy values', () => {
+        const array = [false, 42, 0, '', true, null, 'hello'];
+        expect(_.compact(array)).not.toContain(false);
+        expect(_.compact(array)).not.toContain(0);
+        expect(_.compact(array)).not.toContain('');
+        expect(_.compact(array)).not.toContain(null);
+    });
+});

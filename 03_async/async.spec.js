e9688231e1b398673a7e054dd66719c1cbd6c8c3
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-13 00:30:48 +0300

    write async tests

---
+++ 03_async/async.spec.js
@@ -0,0 +1,28 @@
+const Ajax = require('./async');
+
+describe('Ajax: echo', () => {
+    test('should return value async', async () => {
+        const result = await Ajax.echo('some data');
+        expect(result).toBe('some data');
+    });
+
+    test('should return value with promise', () => {
+        return Ajax.echo('some data').then(data => {
+            expect(data).toBe('some data');
+        });
+    });
+
+    test('should return value with promise_1', () => {
+        return Ajax.echo().catch(err => {
+            expect(err).toBeInstanceOf(Error);
+        });
+    });
+
+    test('should return value with promise_2', async () => {
+        try{
+            await Ajax.echo();
+        } catch(e) {
+            expect(e.message).toBe('error');
+        }
+    });
+});
